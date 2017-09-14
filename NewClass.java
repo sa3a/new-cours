b0590fc0de7d5ca2a779503314a5133d0e7b9083
Sabuhi <sabuhi-amirov@mail.ru>
2017-09-14 18:15:09 +0500

    add files

---
+++ NewClass.java
@@ -0,0 +1,8 @@
+Êþº¾   2 "  NewClass  java/lang/Object <init> ()V Code
+  	   LineNumberTable LocalVariableTable this 
+LNewClass; Main ([Ljava/lang/String;)V	    java/lang/System   out Ljava/io/PrintStream;  Hello Git!!!
+    java/io/PrintStream   println (Ljava/lang/String;)V args [Ljava/lang/String; 
+SourceFile NewClass.java !               /     *· ±    
+                    	       7     	² ¶ ±    
+   
+              	           !
