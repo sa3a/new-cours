9c8ab2056dd1c95775c1d28cec308e13028787dc
Sabuhi <sabuhi-amirov@mail.ru>
2017-09-15 01:56:03 +0500

    commit in to branch master

--- NewClass.java
+++ NewClass.java
@@ -2,7 +2,8 @@
 
 
  java/lang/Object <init> ()V Code
-  	   LineNumberTable LocalVariableTable this 
+  
+"branch master"	   LineNumberTable LocalVariableTable this 
 LNewClass; Main ([Ljava/lang/String;)V	    java/lang/System   out Ljava/io/PrintStream;  Hello Git!!!
     java/io/PrintStream   println (Ljava/lang/String;)V args [Ljava/lang/String; 
 SourceFile NewClass.java !               /     *· ±    
